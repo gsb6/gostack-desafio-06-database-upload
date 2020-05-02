9379349c112d51eee795e0d7812d057add84cd2d
Gabriel Brunichaki <6abriel.dev@gmail.com>
2020-05-02 20:21:37 -0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import 'reflect-metadata';
+import 'express-async-errors';
 import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 import AppError from './errors/AppError';
@@ -12,6 +13,7 @@ import createConnection from './database';
 createConnection();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
